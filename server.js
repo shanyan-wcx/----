6e16a4fe3417bd29e7fba071697cb5cc739def8d
shanyan-wcx <2163304747@qq.com>
2022-12-04 15:57:11 +0800

    update

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ serveHTTP(addonInterface, { port: process.env.PORT || 52696 })
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
+publishToCentral("https://2b8facee70bc-dmhy.baby-beamup.club/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
